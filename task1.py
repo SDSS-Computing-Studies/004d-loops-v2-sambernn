1e6d4e941be1609bb2b80339a99c71b5f30bd7c1
sambernn <55156541+sambernn@users.noreply.github.com>
2020-10-01 13:08:09 -0700

    w

---
+++ task1.py
@@ -0,0 +1,5 @@
+x = input("Enter a number:")
+x = int(x)
+for i in range(1,13):
+    i = x * i
+    print(i,end=" ")
---
+++ task2.py
@@ -0,0 +1,5 @@
+a = input("Enter a name:")
+names = ["Joe", "Jeff", "John"]
+for x in names:
+    if a == x:
+        print("match")
