24c516423181c30eabeacd095f41597095610bb2
sambernn <55156541+sambernn@users.noreply.github.com>
2020-10-05 12:40:03 -0700

    e

--- task2.py
+++ task2.py
@@ -3,3 +3,5 @@ names = ["Joe", "Jeff", "John"]
 for x in names:
     if a == x:
         print("match")
+    else:
+        print("That name is not on the list")
--- task3.py
+++ task3.py
@@ -1,4 +1,4 @@
-numList = (5, 7, 11, 15, 27, 35)
+numList = (25, 8, 10, 11, 33, 30, 51, 75, 63, 14, 20, 99)
 
 for i in numList:
     numList = i/5
